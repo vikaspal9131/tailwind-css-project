13aaaa3d49d3b908cfc04b062e17fc1399fbd50e
vikaspal9131 <vikaspal968562@gmail.com>
2024-04-13 15:49:14 +0530

    update the hero section

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,12 @@ module.exports = {
   content: ["./dist/index.html"],
   theme: {
     extend: {},
+    fontFamily: {
+      vigaa:["Viga","sans-serif"]
+    },
+    color: {
+      'custom-blue': '#2a224b',
+    }
   },
   plugins: [],
 }
