daac3d416fab7ae9a2e297d26e68a1b48ae1155f
vikaspal9131 <vikaspal968562@gmail.com>
2024-04-20 01:53:28 +0530

    add featur page

--- dist/script.js
+++ dist/script.js
@@ -1,3 +1,42 @@
 function handleMenu(){
   console.log("hello word")
 }
+
+
+function textChange(){
+  var typed = new Typed('#auto-type', {
+    strings: ['of Frontend Development ', 'Inhance the user expirence' , 'changeable According to us' ],
+    typeSpeed: 100,
+    backspeed: 100,
+    loop : true,
+    });
+}
+
+
+// js animatin 
+AOS.init({
+  offset: 100,
+  duration: 500,
+});
+
+
+
+textChange();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
